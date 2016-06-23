3f20ea202afb07473b9871366b4c3e441ce80002
wartalker <wartalker@gmail.com>
2016-06-23 23:53:47 +0800

    refactor

--- mongodb/init_admin.js
+++ mongodb/init_admin.js
@@ -1,8 +1,14 @@
-var conn = new Mongo();
+
+/* password admin */
+var admin_pwd="admin"
+
 var db = null
 
-/* admin */
+/* connect to mongodb */
+var conn = new Mongo();
+
+/* create admin user */
 db = conn.getDB("admin")
 db.createUser({user: "admin",
-	       pwd: "admin",
+	       pwd: admin_pwd,
 	       roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]})
--- mongodb/init_db.js
+++ mongodb/init_db.js
@@ -1,4 +1,9 @@
 
+/* password */
+var scrapy_pwd="scrapy"
+var flask_pwd="flask"
+
+/* connect to admin */
 var conn = new Mongo();
 var db = null
 
@@ -8,15 +13,15 @@ db.auth("admin", "admin")
 /* articles db user */
 db = conn.getDB("articles");
 db.createUser({user: "scrapy",
-	       pwd: "scrapy",
+	       pwd: scrapy_pwd,
 	       roles: [ { role: "readWrite", db: "articles" } ]})
 
 db.createUser({user: "flask",
-	       pwd: "flask",
+	       pwd: flask_pwd,
 	       roles: [ { role: "read", db: "articles" } ]})
 
 /* score db user */
 db = conn.getDB("score");
 db.createUser({user: "flask",
-	       pwd: "flask",
+	       pwd: flask_pwd,
 	       roles: [ { role: "readWrite", db: "score" } ]})
