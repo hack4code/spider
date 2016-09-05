9a8ef4bf4e81334d2cf213010c7b50fc13e8a468
wartalker <wartalker@gmail.com>
2016-09-05 10:14:52 +0800

    feed

--- www/f/feed.py
+++ www/f/feed.py
@@ -3,21 +3,17 @@
 
 from flask import Blueprint, render_template
 
-from model import get_categories
-
 
 feed_page = Blueprint('feed_page',
                       __name__,
                       template_folder='template')
 
 
-@feed_page.route('/atom', methods=['GET'])
+@feed_page.route('/rss', methods=['GET'])
 def submit_atom():
-    categories = get_categories()
-    return render_template('atom.html', categories=categories)
+    return render_template('rss.html')
 
 
 @feed_page.route('/blog', methods=['GET'])
 def submit_blog():
-    categories = get_categories()
-    return render_template('blog.html', categories=categories)
+    return render_template('blog.html')
