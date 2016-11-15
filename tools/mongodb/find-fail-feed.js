ae275fd562defca0a6c2b47cb1f9820f151d1cd3
wartalker <wartalker@gmail.com>
2016-11-15 12:01:34 +0800

    fix bug

--- tools/mongodb/find-fail-feed.js
+++ tools/mongodb/find-fail-feed.js
@@ -2,10 +2,10 @@ const user = "flask";
 const passwd = "flask";
 
 const conn = new Mongo();
-const db = conn.getDB("articles");
+var db = conn.getDB("articles");
 db.auth(user, passwd);
 
-const cursor = db.feed.find({});
+var cursor = db.feed.find({});
 while (cursor.hasNext()) {
 	printjson(cursor.next());
 }
