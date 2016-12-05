b6713308e4634d2a44a0de93b5d855d9e0f51825
wartalker <wartalker@gmail.com>
2016-12-05 10:25:14 +0800

    fix bug: remove count pipeline

--- spider/mydm/pipelines/__init__.py
+++ spider/mydm/pipelines/__init__.py
@@ -4,5 +4,4 @@
 from .content import ContentPipeline
 from .image import ImagesDlownloadPipeline
 from .store import StorePipeline
-from .count import CountPipeline
 from .stats import StatsPipeline
