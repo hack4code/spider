4fbcb9af806a14e9115dc7976e68deef1233aa29
wartalker <wartalker@gmail.com>
2016-12-15 23:39:07 +0800

    clean code

--- spider/mydm/util.py
+++ spider/mydm/util.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
 
-try:
-    from urllib.parse import urlparse
-except ImportError:
-    from urlparse import urlparse
+from urllib.parse import urlparse
 from collections import namedtuple
 
 
-RedisConf = namedtuple('RedisUrl', ['host', 'port', 'database'])
+RedisConf = namedtuple('RedisUrl',
+                       ['host', 'port', 'database'])
 
 
 def parse_redis_url(url):
@@ -16,4 +14,6 @@ def parse_redis_url(url):
     host = parser.hostname
     port = parser.port
     db = int(parser.path[1:])
-    return RedisConf(host, port, db)
+    return RedisConf(host,
+                     port,
+                     db)
