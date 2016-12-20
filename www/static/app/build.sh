ed87da0b5e36339586d12b37746c6b81ced65cdb
wartalker <wartalker@gmail.com>
2016-12-21 03:34:31 +0800

    npm install

--- www/static/app/build.sh
+++ www/static/app/build.sh
@@ -1,12 +1,14 @@
-#! /bin/bash
+#!/bin/bash
 
 npm init
 npm install react --save-dev
 npm install react-dom --save-dev
+npm install jquery --save-dev
 npm install babel-core --save-dev
 npm install babel-preset-es2015 --save-dev
 npm install babel-preset-react --save-dev
+npm install babel-preset-stage-0 --save-dev
 npm install babel-loader --save-dev
-npm install webpack -g
+npm install webpack --save-dev
 
-webpack
+npm run build
