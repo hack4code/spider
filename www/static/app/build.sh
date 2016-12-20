a375eba3d6be240e0f01f39124467e8816807cab
wartalker <wartalker@gmail.com>
2016-12-20 20:03:39 +0800

    pack js script

---
+++ www/static/app/build.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+npm init
+npm install react --save-dev
+npm install react-dom --save-dev
+npm install babel-core --save-dev
+npm install babel-preset-es2015 --save-dev
+npm install babel-preset-react --save-dev
+npm install babel-loader --save-dev
+npm install webpack -g
+
+webpack
