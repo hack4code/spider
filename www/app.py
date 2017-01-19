0e5976993438fe13a3fd551c58645ad189aca5aa
hack4code <code4hack+github@gmail.com>
2017-01-19 11:35:02 +0800

    refactor

--- www/app.py
+++ www/app.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
 
+from datetime import datetime
+from collections import namedtuple
+
 from flask import Flask, render_template
+
 from error import NotFound, BadRequest
+from user import need_uid
 
 # DEBUG = True
 
@@ -45,17 +50,71 @@ from util import DateConverter, IdConverter
 app.url_map.converters['date'] = DateConverter
 app.url_map.converters['id'] = IdConverter
 
-from home import home_page
-app.register_blueprint(home_page, url_prefix='/home')
-from a import article_page
-app.register_blueprint(article_page, url_prefix='/a')
-from p import spider_page
-app.register_blueprint(spider_page, url_prefix='/p')
-from d import entry_page
-app.register_blueprint(entry_page, url_prefix='/d')
-from l import list_page
-app.register_blueprint(list_page, url_prefix='/l')
-from f import feed_page
-app.register_blueprint(feed_page, url_prefix='/f')
+
+@app.route('/', methods=['GET'])
+def home():
+    today = str(datetime.utcnow().date())
+    return show_entries_byday(today)
+
+
+@app.route('/d/<date:day>', methods=['GET'])
+@need_uid
+def show_entries_byday(day):
+    if day is None:
+        raise BadRequest('invalid day')
+    return render_template('day.html',
+                           day_entry=day)
+
+
+@app.route('/a/<id:aid>', methods=['GET'])
+def show_article(aid):
+    if aid is None:
+        raise BadRequest('invalid article id')
+
+    from model import get_article
+    a = get_article(aid)
+    if a is None:
+        raise NotFound('article for id={} not exist'.format(aid))
+
+    def get_css(dom):
+        import os
+        path = '{}/css/{}.css'.format(app.static_folder,
+                                      dom)
+        return '{}.css'.format(dom) if os.path.isfile(path) else None
+
+    return render_template('article.html',
+                           article=a,
+                           dom_css=get_css(a.domain))
+
+
+@app.route('/p/<id:spid>', methods=['GET'])
+def show_entries_byspider(spid):
+    from model import get_spiders
+    Spider = namedtuple('Spider', ['id', 'source'])
+    if spid is None:
+        raise BadRequest('invalid spider id')
+    spid = str(spid)
+    spiders = get_spiders()
+    if spid not in spiders:
+        raise BadRequest('spider id not existed')
+    return render_template('entries.html',
+                           spider=Spider(spid,
+                                         spiders[spid]))
+
+
+@app.route('/l/p', methods=['GET'])
+def list_spiders():
+    return render_template('spiders.html')
+
+
+@app.route('/feed/rss', methods=['GET'])
+def submit_atom():
+    return render_template('rss.html')
+
+
+@app.route('/feed/blog', methods=['GET'])
+def submit_blog():
+    return render_template('blog.html')
+
 from api import api_page
 app.register_blueprint(api_page, url_prefix='/api')
