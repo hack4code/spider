653ab23d16cf96af06a4ee2ff013c12168a5adfb
hack4code <code4hack+github@gmail.com>
2017-01-22 13:11:18 +0800

    remove check_aid

--- www/model/__init__.py
+++ www/model/__init__.py
@@ -3,4 +3,4 @@ from model.mongodb import get_begin_day, get_end_day, get_entries, \
     get_first_aid, get_entries_pre, get_entries_next, \
     get_entries_spider, get_article, get_all_days, \
     get_all_articles, get_max_aid_all, get_articles, \
-    check_aid, vote_article, get_categories
+    vote_article, get_categories
