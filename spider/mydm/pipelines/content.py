02a443c2429abd277e80d85ef072eaf139b144a7
hack4code <code4hack+github@gmail.com>
2017-01-29 13:06:33 +0800

    xpath

--- spider/mydm/pipelines/content.py
+++ spider/mydm/pipelines/content.py
@@ -131,10 +131,8 @@ class ContentPipeline(object):
             for e in doc.xpath('//a/div'):
                 e.drop_tree()
 
-            for e in doc.xpath('//div[not(descendant-or-self::div) and '
-                               'not(descendant-or-self::img)]'):
-                text = e.text_content().strip(' \r\n\t')
-                if not text:
+            for e in doc.xpath('//div'):
+                if len(e) == 0:
                     e.drop_tree()
 
         remove_tags(doc)
