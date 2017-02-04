9d99117ca6a215c532170c12c9e8eee4e81b2455
hack4code <code4hack+github@gmail.com>
2017-02-04 20:21:17 +0800

    removed_xpath_nodes

--- www/static/app/blog.js
+++ www/static/app/blog.js
@@ -36,7 +36,8 @@ class SubmitForm extends React.Component {
     else {
       this.err.fadeIn("正在提交 .....");
 
-      let nodes = form["removed_xpath_nodes"].filter((node) => {node != ""});
+      let nodes = form["removed_xpath_nodes"].filter((e, index, array) => {e != ""});
+      console.log(nodes);
       if (nodes.length > 0) {
         form["removed_xpath_nodes"] = JSON.stringify(nodes);
       }
