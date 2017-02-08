d40531c9e8b4569bd0285455aef750956057ef2c
code4hack <code4hack@gmail.com>
2017-02-08 17:50:12 +0800

    log dns http error

--- spider/mydm/spider/spider.py
+++ spider/mydm/spider/spider.py
@@ -2,12 +2,13 @@
 
 
 import logging
-
-from twisted.internet.error import TimeoutError
+from urllib.parse import urlparse
 
 from scrapy.spiders import Spider
 from scrapy import Request
-
+from scrapy.spidermiddlewares.httperror import HttpError
+from twisted.internet.error import DNSLookupError
+from twisted.internet.error import TimeoutError, TCPTimedOutError
 
 logger = logging.getLogger(__name__)
 
@@ -21,8 +22,18 @@ class ErrbackSpider(Spider):
                           errback=self.errback)
 
     def errback(self, failure):
-        if failure.check(TimeoutError):
+        if failure.check(DNSLookupError):
+            host = urlparse(failure.request.url).hostname
+            logger.error('DNSLookupError on domain[%s]',
+                         host)
+        elif failure.check(TimeoutError, TCPTimedOutError):
             request = failure.request
-            logger.error('TimeoutError on {}'.format(request))
+            logger.error('TimeoutError on url[%s]',
+                         request.url)
+        elif failure.check(HttpError):
+            response = failure.value.response
+            logger.error('HttpError on url[%s, code=%d]',
+                         response.url,
+                         response.code)
         else:
             logger.error(repr(failure))
