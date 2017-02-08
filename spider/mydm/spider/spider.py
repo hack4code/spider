246c0329866c69a7974a75ae12bd85f15c67878f
code4hack <code4hack@gmail.com>
2017-02-08 18:04:16 +0800

    logger

--- spider/mydm/spider/spider.py
+++ spider/mydm/spider/spider.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-import logging
 from urllib.parse import urlparse
 
 from scrapy.spiders import Spider
@@ -9,8 +8,6 @@ from scrapy import Request
 from scrapy.spidermiddlewares.httperror import HttpError
 from twisted.internet.error import DNSLookupError
 from twisted.internet.error import TimeoutError, TCPTimedOutError
-
-logger = logging.getLogger(__name__)
 
 
 class ErrbackSpider(Spider):
@@ -24,16 +21,16 @@ class ErrbackSpider(Spider):
     def errback(self, failure):
         if failure.check(DNSLookupError):
             host = urlparse(failure.request.url).hostname
-            logger.error('DNSLookupError on domain[%s]',
-                         host)
+            self.logger.error('DNSLookupError on domain[%s]',
+                              host)
         elif failure.check(TimeoutError, TCPTimedOutError):
             request = failure.request
-            logger.error('TimeoutError on url[%s]',
-                         request.url)
+            self.logger.error('TimeoutError on url[%s]',
+                              request.url)
         elif failure.check(HttpError):
             response = failure.value.response
-            logger.error('HttpError on url[%s, code=%d]',
-                         response.url,
-                         response.code)
+            self.logger.error('HttpError on url[%s, code=%d]',
+                              response.url,
+                              response.code)
         else:
-            logger.error(repr(failure))
+            self.logger.error(repr(failure))
