4d9f802308aae5f1e4b85f5ede179dffd031f223
code4hack <code4hack@gmail.com>
2017-03-14 16:17:35 +0800

    lxml tag extractor

--- spider/mydm/ai/tag.py
+++ spider/mydm/ai/tag.py
@@ -14,32 +14,17 @@ class ReExtractor:
     p = re.compile(r'(tags\s*:|Fileds?\s*under\s*:|Tagged\s*with)\s*(.*)',
                    re.IGNORECASE)
 
-    def __call__(self, doc, encoding='UTF-8'):
-        if isinstance(doc,
-                      (str, bytes)):
-            doc = fromstring(bytes(bytearray(doc,
-                                             encoding=encoding)),
-                             parser=HTMLParser(encoding=encoding))
-        if not isinstance(doc,
-                          HtmlElement):
-            return None
-
+    def __call__(self, doc):
         matches = self.p.findall(doc.text_content())
 
         def extract(s):
             s = re.sub(r'(\s|\t|\r|\n)+',
                        ' ',
                        s)
-            tags_ = []
+            tags = []
             for _ in s.split(','):
                 tag = _.strip()
                 if tag:
-                    tags_.append(tag)
-            tags = []
-            for index, tag in enumerate(tags_):
-                if index < 2 and len(tag) > 16:
-                    return None
-                elif len(tag) < 16:
                     tags.append(tag)
             return tags
 
@@ -64,18 +49,51 @@ class ReExtractor:
                 return tags
 
 
+class LXMLExtractor:
+    p = re.compile(r'^\s*(tags\s*:|Fileds?\s*under\s*:|Tagged\s*with)\s*$',
+                   re.IGNORECASE)
+
+    def __call__(self, doc):
+        for e in doc.xpath('//*[count(child::*)=0]'):
+            if self.p.match(e.text_content()) is not None:
+                container = e.getparent()
+                for idx, _ in enumerate(container):
+                    if _ is e:
+                        break
+                tags = []
+                for _ in container[idx+1:]:
+                    tag = _.text_content().strip('\r\n\t\s')
+                    if tag:
+                        tags.append(tag)
+                return tags
+
+
 class TagExtractor:
-    EXTRACTORS = (ReExtractor,)
+    EXTRACTORS = (ReExtractor, LXMLExtractor)
 
     def __call__(self, doc, encoding='UTF-8'):
+        if isinstance(doc,
+                      (str, bytes)):
+            doc = fromstring(bytes(bytearray(doc,
+                                             encoding=encoding)),
+                             parser=HTMLParser(encoding=encoding))
+        if not isinstance(doc,
+                          HtmlElement):
+            return None
         for cls in self.EXTRACTORS:
-            match = cls()
-            tags = match(doc,
-                         encoding=encoding)
-            if tags:
-                logger.info('TagExtractor got tags %s',
-                            tags)
-                return tags
+            tags = []
+            extract = cls()
+            tags_ = extract(doc)
+            for idx, tag in enumerate(tags_):
+                if idx < 2 and len(tag) > 16:
+                    break
+                elif len(tag) < 16:
+                    tags.append(tag)
+            else:
+                if tags:
+                    logger.info('TagExtractor got tags %s',
+                                tags)
+                    return tags
 
 
 def extract_tags(doc, encoding):
