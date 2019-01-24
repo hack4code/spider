7d645dc36603e97cc4e474db8bcdcd4c6e48fa04
hack4code <code4hack@gmail.com>
2019-01-24 10:09:33 +0000

    react 7

--- www/static/app/build.sh
+++ www/static/app/build.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /usr/bin/env bash
 
 npm init -f
 
@@ -7,13 +7,12 @@ npm install react-dom --save-dev
 npm install react-transition-group --save-dev
 npm install whatwg-fetch --save-dev
 npm install @babel/core --save-dev
-npm install babel-preset-env --save-dev
-npm install babel-preset-react --save-dev
-npm install babel-preset-stage-0 --save-dev
-npm install uglifyjs-webpack-plugin --save-dev
+npm install @babel/preset-env --save-dev
+npm install @babel/preset-react --save-dev
+npm install @babel/plugin-proposal-class-properties --save-dev
 npm install webpack --save-dev
 npm install webpack-cli --save-dev
 npm install babel-loader --save-dev
+npm install uglifyjs-webpack-plugin --save-dev
 
-node_modules/.bin/webpack --config webpack.config.js
-
+node_modules/.bin/webpack --mode production --config webpack.config.js
