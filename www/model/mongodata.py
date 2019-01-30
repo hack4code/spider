4551901ecad2a4b1d2a962898b4865e3ff6257b9
hack4code <code4hack@gmail.com>
2019-01-30 16:31:26 +0800

    format

--- www/model/mongodata.py
+++ www/model/mongodata.py
@@ -5,49 +5,60 @@ from html import unescape
 from collections import namedtuple
 
 
+AID = namedtuple('AID', ['id'])
 EntryBase = namedtuple('Entry', ['id', 'title'])
 
 
 class Entry(EntryBase):
 
     def __new__(cls, d):
-        return super().__new__(cls, str(d['_id']), d.get('title'))
+        return super().__new__(
+                cls,
+                str(d['_id']),
+                d.get('title')
+        )
 
 
-EntryDayBase = namedtuple('EntryDay',
-                          ['id',
-                           'title',
-                           'category',
-                           'source',
-                           'tag',
-                           'spider',
-                           'domain',
-                           'link'])
+EntryDayBase = namedtuple(
+        'EntryDay',
+        ['id',
+         'title',
+         'category',
+         'source',
+         'tag',
+         'spider',
+         'domain',
+         'link']
+)
 
 
 class EntryDay(EntryDayBase):
 
     def __new__(cls, d):
-        return super().__new__(cls,
-                               str(d['_id']),
-                               d.get('title'),
-                               d.get('category'),
-                               d.get('source'),
-                               d.get('tag'),
-                               d.get('spider'),
-                               d.get('domain'),
-                               d.get('link'))
+        return super().__new__(
+                cls,
+                str(d['_id']),
+                d.get('title'),
+                d.get('category'),
+                d.get('source'),
+                d.get('tag'),
+                d.get('spider'),
+                d.get('domain'),
+                d.get('link')
+        )
 
 
-ArticleBase = namedtuple('Article',
-                         ['id',
-                          'title',
-                          'domain',
-                          'link',
-                          'content',
-                          'lang',
-                          'source',
-                          'spider'])
+ArticleBase = namedtuple(
+        'Article',
+        ['id',
+         'title',
+         'domain',
+         'link',
+         'content',
+         'lang',
+         'source',
+         'spider']
+)
 
 
 class Article(ArticleBase):
@@ -57,15 +68,14 @@ class Article(ArticleBase):
         if isinstance(content, bytes):
             content = content.decode('UTF-8')
         title = unescape(d.get('title'))
-        return super().__new__(cls,
-                               str(d['_id']),
-                               title,
-                               d.get('domain'),
-                               d.get('link'),
-                               content,
-                               d.get('lang'),
-                               d.get('source'),
-                               d.get('spider'))
-
-
-AID = namedtuple('AID', ['id'])
+        return super().__new__(
+                cls,
+                str(d['_id']),
+                title,
+                d.get('domain'),
+                d.get('link'),
+                content,
+                d.get('lang'),
+                d.get('source'),
+                d.get('spider')
+        )
