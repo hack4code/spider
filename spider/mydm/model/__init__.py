85e9af01c36f9d8d2f4450b4002ae82319a940b6
hack4code <code4hack@gmail.com>
2019-01-30 11:07:29 +0800

    format

--- spider/mydm/model/__init__.py
+++ spider/mydm/model/__init__.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 
-from .mongodb import is_exists_feed, save_feed, is_exists_article, \
-        is_exists_spider, \
-        save_article, save_spider_settings, \
-        update_spider_stats, \
+from .mongodb import (
+        is_exists_feed, save_feed, is_exists_article,
+        is_exists_spider,
+        save_article, save_spider_settings,
+        update_spider_stats,
         get_spider_settings, get_category_tags
+)
 
 
 __all__ = [
