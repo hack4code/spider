51538e68e7d64fbdaad6b073e77f5fcaee10876d
hack4code <code4hack@gmail.com>
2019-01-30 11:14:58 +0800

    stats

--- spider/mydm/extensions/stats.py
+++ spider/mydm/extensions/stats.py
@@ -6,7 +6,6 @@ import logging
 from scrapy import signals
 
 from ..util import save_stats
-from ..model import update_spider_stats
 
 
 logger = logging.getLogger(__name__)
@@ -19,13 +18,18 @@ class ExtensionStats:
     @classmethod
     def from_crawler(cls, crawler):
         ext = cls(crawler.stats)
-        crawler.signals.connect(ext.spider_opened,
-                                signal=signals.spider_opened)
-        crawler.signals.connect(ext.spider_closed,
-                                signal=signals.spider_closed)
-        crawler.signals.connect(ext.item_scraped,
-                                signal=signals.item_scraped)
-
+        crawler.signals.connect(
+                ext.spider_opened,
+                signal=signals.spider_opened
+        )
+        crawler.signals.connect(
+                ext.spider_closed,
+                signal=signals.spider_closed
+        )
+        crawler.signals.connect(
+                ext.item_scraped,
+                signal=signals.item_scraped
+        )
         return ext
 
     def spider_opened(self, spider):
@@ -35,13 +39,16 @@ class ExtensionStats:
         pass
 
     def spider_closed(self, spider):
-        value = self.stats.get_value('item_scraped_count', 0)
-        save_stats(spider.settings['SPIDER_STATS_URL'],
-                   spider._id,
-                   value)
-        if spider.settings['BOT_NAME'] != 'TestSpider':
-            logger.info('spider[%s] crawled %d articles',
-                        spider.name,
-                        value)
-            if value == 0:
-                update_spider_stats(spider, {'fail': 1})
+        item_scraped_count = self.stats.get_value('item_scraped_count', 0)
+        save_stats(
+                spider.settings['SPIDER_STATS_URL'],
+                spider._id,
+                item_scraped_count
+        )
+        if spider.settings['BOT_NAME'] == 'TestSpider':
+            return
+        logger.info(
+                'spider[%s] crawled %d articles',
+                spider.name,
+                item_scraped_count
+        )
