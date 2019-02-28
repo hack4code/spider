772e8c4cfc0d4a6d2525e1ac6b20cd2268aea7b7
hack4code <code4hack@gmail.com>
2019-02-28 12:55:49 +0800

    mongo script

---
+++ mongo/script/findspider.js
@@ -0,0 +1,13 @@
+
+const user = "flask";
+const passwd = "flask";
+
+var conn = new Mongo();
+var db = conn.getDB("scrapy");
+db.auth(user, passwd);
+
+var cursor = db.spider.find({"start_urls": {$regex: regex}});
+while (cursor.hasNext()) {
+	spider = cursor.next();
+	printjson(spider);
+}
---
+++ mongo/script/rmspider.js
@@ -0,0 +1,10 @@
+
+
+const user = "scrapy";
+const passwd = "scrapy";
+
+var conn = new Mongo();
+var db = conn.getDB("scrapy");
+db.auth(user, passwd);
+
+db.spider.remove({_id: ObjectId(spid)}) 
