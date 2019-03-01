743bda2549ace239a305539da5fd275b39ae5fbc
hack4code <code4hack@gmail.com>
2019-03-01 18:00:36 +0800

    refactor xml spider meta class

--- spider/src/mydm/spider/xml/spider.py
+++ spider/src/mydm/spider/xml/spider.py
@@ -22,10 +22,13 @@ logger = logging.getLogger(__name__)
 
 
 class LXMLSpider(Spider):
-
-    # attributes must contain
-    ATTRS = ('title', 'link')
-
+    """ spider attribute:
+        start_urls
+        category
+        name
+        item_content_xpath
+        item_content_link_parameter
+    """
     def extract_tags(self, item):
         if 'tag' in item:
             return
@@ -71,39 +74,39 @@ class LXMLSpider(Spider):
             while len(root) == 1:
                 root = root[0]
             for entry in root:
-                extract = ItemExtractor()
-                item = extract(entry)
+                item = ItemExtractor()(entry)
                 item['category'] = self.category
                 item['crawl_date'] = datetime.now()
                 item['domain'] = urlparse(response.request.url).netloc
                 item['data_type'] = 'html'
                 item['encoding'] = response.encoding
-                if all(item.get(attr) is not None for attr in self.ATTRS):
-                    self.extract_tags(item)
-                    if hasattr(self, 'item_content_xpath'):
-                        try:
-                            link = (f'{item["link"]}?'
-                                    '{self.item_content_link_parameter}')
-                        except AttributeError:
-                            link = item['link']
-                        yield Request(
-                                link,
-                                meta={'item': item},
-                                callback=self.extract_content,
-                                errback=self.errback,
-                                dont_filter=True
-                        )
-                    elif item.get('content'):
-                        yield ArticleItem(item)
-
-
-XMLSPIDER_ATTRS = ['start_urls', 'category', 'name']
+                if any(item.get(attr) is None for attr in ('title', 'link')):
+                    continue
+                self.extract_tags(item)
+                if hasattr(self, 'item_content_xpath'):
+                    try:
+                        params = self.item_content_link_parameter
+                    except AttributeError:
+                        link = item['link']
+                    else:
+                        link = f'{item["link"]}?{params}'
+                    yield Request(
+                            link,
+                            meta={'item': item},
+                            callback=self.extract_content,
+                            errback=self.errback,
+                            dont_filter=True
+                    )
+                elif item.get('content'):
+                    yield ArticleItem(item)
 
 
 class LXMLSpiderMeta(SpiderFactory, type, spider_type='xml'):
 
+    SPIDER_ATTRS = ('start_urls', 'category', 'name')
+
     def __new__(cls, name, bases, attrs):
-        if all(attr in attrs for attr in XMLSPIDER_ATTRS):
+        if all(attr in attrs for attr in cls.SPIDER_ATTRS):
 
             def update_bases(bases):
                 new_bases = [LXMLSpider]
@@ -122,7 +125,7 @@ class LXMLSpiderMeta(SpiderFactory, type, spider_type='xml'):
         else:
             miss_attrs = [
                     attr
-                    for attr in XMLSPIDER_ATTRS
+                    for attr in cls.SPIDER_ATTRS
                     if attr not in attrs
             ]
             raise AttributeError(f'miss attributes{miss_attrs}')
