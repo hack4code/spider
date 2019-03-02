9606d7588d3df23adeb8c9ab2d843528a94d9056
hack4code <code4hack@gmail.com>
2019-03-02 11:50:01 +0000

    findspider support name

--- mongo/script/findspider.js
+++ mongo/script/findspider.js
@@ -6,9 +6,19 @@ var conn = new Mongo();
 var db = conn.getDB("scrapy");
 db.auth(user, passwd);
 
-var cursor = db.spider.find({"start_urls": {$regex: regex}});
-while (cursor.hasNext()) {
-	spider = cursor.next();
-	printjson(spider);
-	print(" ");
+var cursor = db.spider.find({start_urls: {$regex: regex}});
+if (cursor.count() > 0) {
+	while (cursor.hasNext()) {
+		spider = cursor.next();
+		printjson(spider);
+		print(" ");
+	}
+}
+else {
+	var cursor = db.spider.find({name: {$regex: regex}});
+	while (cursor.hasNext()) {
+		spider = cursor.next();
+		printjson(spider);
+		print(" ");
+	}
 }
