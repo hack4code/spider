0ce95589c4b7bc74da81b81299e164cc2b9e5866
hack4code <code4hack@gmail.com>
2019-03-03 11:57:24 +0800

    refactor image pipeline

--- spider/src/mydm/pipelines/image.py
+++ spider/src/mydm/pipelines/image.py
@@ -3,6 +3,7 @@
 
 import base64
 import logging
+import tempfile
 from io import BytesIO
 from urllib.parse import urlparse, urljoin
 
@@ -25,11 +26,16 @@ class Image:
 
     MAX_WIDTH = 1024
 
-    def __init__(self, data):
-        self._image = ImageLib.open(BytesIO(data))
-
-    def close(self):
-        self._image.close()
+    def __init__(self, data, type=None):
+        try:
+            self._image = ImageLib.open(BytesIO(data))
+        except OSError:
+            if type not in ('PNG', 'JPG', 'JPEG'):
+                raise
+            fp = tempfile.NamedTemporaryFile(suffix=f'.{type}')
+            fp.write(data)
+            fp.seek(0)
+            self._image = ImageLib.open(fp)
 
     @property
     def size(self):
@@ -43,8 +49,9 @@ class Image:
         image = self._image
         width, height = image.size
         if width > self.MAX_WIDTH:
-            height = int(float(height)/width*self.MAX_WIDTH)
+            ratio = float(height) / float(width)
             width = self.MAX_WIDTH
+            height = int(width * ratio)
             image = image.resize(
                     (width, height),
                     ImageLib.ANTIALIAS
@@ -87,18 +94,19 @@ class ImagesDlownloadPipeline(MediaPipeline):
         return True
 
     def get_media_requests(self, item, info):
+        self._invalid_img_element = []
         doc = item['content']
         assert isinstance(doc, HtmlElement)
         attrs = {'src'}
-        imgattr = getattr(
+        img_attr = getattr(
                 self.spider,
                 'image_url_attr',
                 None,
         )
-        if isinstance(imgattr, (list, tuple)):
-            attrs = attrs.union(imgattr)
-        elif imgattr:
-            attrs.add(imgattr)
+        if isinstance(img_attr, (list, tuple)):
+            attrs = attrs.union(img_attr)
+        elif img_attr:
+            attrs.add(img_attr)
 
         urls = []
         for e in doc.xpath('//img'):
@@ -118,7 +126,7 @@ class ImagesDlownloadPipeline(MediaPipeline):
                     break
             else:
                 logger.error(
-                        "spider[%s] can't find link attribute of image",
+                        "spider[%s] can't find image link attribute",
                         self.spider_name
                 )
 
@@ -127,9 +135,13 @@ class ImagesDlownloadPipeline(MediaPipeline):
             if url.startswith('data'):
                 continue
             try:
-                r = Request(url, meta={'img': e})
+                r = Request(url, meta={'image_xpath_node': e})
             except ValueError:
-                logger.error('invalid url[%s]', url)
+                logger.error(
+                        'spider[%s] got invalid url[%s]',
+                        self.spider_name,
+                        url
+                )
             else:
                 requests.append(r)
         return requests
@@ -140,7 +152,6 @@ class ImagesDlownloadPipeline(MediaPipeline):
                 self.spider_name,
                 request.url
         )
-        request.meta['img'].set('src', request.url)
 
     def media_downloaded(self, response, request, info):
         if not response.body:
@@ -149,31 +160,42 @@ class ImagesDlownloadPipeline(MediaPipeline):
                     self.spider_name,
                     request.url
             )
+            self._invalid_img_element.append(
+                    response.meta['image_xpath_node']
+            )
             return
-        img = response.meta['img']
+        image_xpath_node = response.meta['image_xpath_node']
         src = response.url
         data = response.body
-        imgsize = len(data)
+        image_size = len(data)
         try:
-            image = Image(data)
-        except OSError as e:
+            image_type = response.headers[
+                    'Content-Type'
+                    ].split('/')[-1].upper()
+        except KeyError:
+            image_type = src.split('.')[-1].upper()
+        try:
+            image = Image(data, type=image_type)
+        except (OSError, IOError) as e:
             logger.error(
                     'spider[%s] PILLOW open image[%s] failed[%s]',
                     self.spider_name,
                     src,
                     e
             )
-            try:
-                imgtype = response.headers['Content-Type'].split('/')[-1]
-            except KeyError:
-                imgtype = src.split('.')[-1]
         else:
-            if self.need_optimize(imgsize):
+            if self.need_optimize(image_size):
                 data = image.optimize()
-            imgtype = image.type
-        img.set('source', src)
+            image_type = image.type.upper()
+        image_xpath_node.set('source', src)
         data = base64.b64encode(data).decode('ascii')
-        img.set('src', f'data:image/{imgtype.upper()};base64,{data}')
+        image_xpath_node.set(
+                'src',
+                f'data:image/{image_type};base64,{data}'
+        )
 
     def item_completed(self, results, item, info):
+        for e in self._invalid_img_element:
+            e.drop_tree()
+        self._invalid_img_element = []
         return item
