4a028dce32af7ee6e8430c5de8a6821b2fc3b74d
code4hack <code4hack+github@gmail.com>
2025-01-18 15:04:39 +0800

    create user flask for www

--- mongo/init/init_db.js
+++ mongo/init/init_db.js
@@ -1,18 +1,22 @@
-/* password */
-const scrapy_pwd="scrapy"
-
 /* connect to admin */
 var conn = new Mongo();
 
 var admin_db = conn.getDB("admin")
 admin_db.auth("admin", "admin")
 
-/* create user scrapy */
 var scrapy_db = conn.getDB("scrapy");
 scrapy_db.createUser(
 	{
 		user: "scrapy",
-		pwd: scrapy_pwd,
+		pwd: "scrapy",
 		roles: [ { role: "readWrite", db: "scrapy" } ]
+	}
+)
+
+scrapy_db.createUser(
+	{
+		user: "flask",
+		pwd: "flask",
+		roles: [ { role: "read", db: "scrapy" } ]
 	}
 )
