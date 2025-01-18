a85eb88dc1a32b258a3ec49d4ccd333c1d05f7fc
code4hack <code4hack+github@gmail.com>
2025-01-18 12:31:24 +0800

    mongo

--- mongo/init/init_db.js
+++ mongo/init/init_db.js
@@ -1,29 +1,18 @@
 /* password */
 const scrapy_pwd="scrapy"
-const flask_pwd="flask"
 
 /* connect to admin */
 var conn = new Mongo();
-var db = null
 
-db = conn.getDB("admin")
-db.auth("admin", "admin")
+var admin_db = conn.getDB("admin")
+admin_db.auth("admin", "admin")
 
 /* create user scrapy */
-db = conn.getDB("scrapy");
-db.createUser(
+var scrapy_db = conn.getDB("scrapy");
+scrapy_db.createUser(
 	{
 		user: "scrapy",
 		pwd: scrapy_pwd,
 		roles: [ { role: "readWrite", db: "scrapy" } ]
-	}
-)
-
-/* create user flask */
-db.createUser(
-	{
-		user: "flask",
-		pwd: flask_pwd,
-		roles: [ { role: "read", db: "scrapy" } ]
 	}
 )
