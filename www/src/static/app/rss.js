efc254ea108382fabfbd3d658f54267c2a2e45ac
code4hack <code4hack+github@gmail.com>
2025-04-05 16:14:38 +0800

    spider

--- www/src/static/app/rss.js
+++ www/src/static/app/rss.js
@@ -1,172 +1,176 @@
-import React from "react";
-import { createRoot } from 'react-dom/client';
-import findDOMNode from "react-dom";
+import {React, useRef} from "react";
+import {createRoot} from 'react-dom/client';
 import {TextBox, SelectBox, EditBox, MEditBox, Button, Title, ErrMsg, Hr} from "./feed_component";
 import "whatwg-fetch";
 
 class SubmitForm extends React.Component {
-  getInitialState() {
-      return {
-        category: "",
-        url: "",
-        item_content_xpath: "",
-        removed_xpath_nodes: ["",],
-        css: ""
-      };
-  }
+    const buttonRef = useRef(null);
 
-  constructor(props) {
-    super(props);
-    this.err = null;
-    this.submit = this.submit.bind(this);
-    this.updateField = this.updateField.bind(this);
-    this.state = this.getInitialState();
-  }
-
-  componentDidMount() {
-    const href = window.location.href;
-    const spid = href.split("/").slice(-1)
-    if ((spid == "rss") || (spid == "xml")) {
-      return;
+    getInitialState() {
+        return {
+            category: "",
+            url: "",
+            item_content_xpath: "",
+            removed_xpath_nodes: ["",],
+            css: ""
+        };
     }
 
-    let that = this;
-    let url = new URL(window.location.protocol + "//" + window.location.hostname + "/api/spider");
-    let params = {spid: spid};
-    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
-    fetch(url).then(function(response) {
-      response.json().then(function(data) {
-        if (response.status == 200) {
-          that.setState({
-            category: data["category"],
-            url: data["start_urls"][0],
-          });
+    constructor(props) {
+        super(props);
+        this.err = null;
+        this.submit = this.submit.bind(this);
+        this.updateField = this.updateField.bind(this);
+        this.state = this.getInitialState();
+    }
 
-          if (("item_content_xpath" in data) && data["item_content_xpath"]) {
-            that.setState({
-              item_content_xpath: data["item_content_xpath"]
-            });
-          };
+    componentDidMount() {
+        const href = window.location.href;
+        const spid = href.split("/").slice(-1)
+        if ((spid == "rss") || (spid == "xml")) {
+            return;
+        }
 
-          if (("removed_xpath_nodes" in data) && data["removed_xpath_nodes"]) {
-            that.setState({
-              removed_xpath_nodes: data["removed_xpath_nodes"]
-            });
-          };
+        let that = this;
+        let url = new URL(window.location.protocol + "//" + window.location.hostname + "/api/spider");
+        let params = {spid: spid};
+        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+        fetch(url).then(function(response) {
+            response.json().then(function(data) {
+                if (response.status == 200) {
+                    that.setState({
+                        category: data["category"],
+                        url: data["start_urls"][0],
+                    });
 
-          if (("css" in data) && data["css"]) {
-            that.setState({
-              css: data["css"]
+                    if (("item_content_xpath" in data) && data["item_content_xpath"]) {
+                        that.setState({
+                            item_content_xpath: data["item_content_xpath"]
+                        });
+                    };
+
+                    if (("removed_xpath_nodes" in data) && data["removed_xpath_nodes"]) {
+                        that.setState({
+                            removed_xpath_nodes: data["removed_xpath_nodes"]
+                        });
+                    };
+
+                    if (("css" in data) && data["css"]) {
+                        that.setState({
+                            css: data["css"]
+                        });
+                    };
+                }
+                else {
+                    console.log(data['message']);
+                }
             });
-          };
+        });
+    }
+
+    submit(e) {
+        e.preventDefault();
+
+        let feed = {}
+        for (let key in this.state) {
+            if (this.state[key].length > 0) {
+                feed[key] = this.state[key];
+            }
+        }
+        if (feed["url"] == null || feed["url"] == "") {
+            this.err.fadeIn("需要网址数据");
+            setTimeout(() => {this.err.fadeOut()}, 800);
         }
         else {
-          console.log(data['message']);
+            this.err.fadeIn("正在提交 .....");
+
+            let item_content_xpath = feed["item_content_xpath"];
+            if (!item_content_xpath) {
+                delete feed["item_content_xpath"];
+            }
+
+            let nodes = feed["removed_xpath_nodes"]
+            if (!nodes) {
+                delete feed["removed_xpath_nodes"];
+            }
+            let css = feed["css"];
+            if (!css) {
+                delete feed["css"]
+            }
+
+            let that = this;
+            fetch("/submit/rss", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(feed)
+            })
+                .then(function(response) {
+                    response.json().then(function(data) {
+                        if (response.status == 200) {
+                            that.err.fadeIn("成功");
+                            setTimeout(() => {that.err.fadeOut()}, 800);
+                            let state = that.getInitialState();
+                            state["category"] = that.state.category;
+                            that.setState(state);
+                        }
+                        else {
+                            that.err.fadeIn("失败: " + data["message"]);
+                            setTimeout(() => {that.err.fadeOut()}, 800);
+                        }
+                    })
+                })
+                .catch(function(err) {
+                    that.err.fadeIn("异常: fetch exception");
+                    setTimeout(() => {that.err.fadeOut()}, 800);
+                })
         }
-     });
-   });
-  }
-
-  submit(e) {
-    e.preventDefault();
-
-    let feed = {}
-    for (let key in this.state) {
-      if (this.state[key].length > 0) {
-        feed[key] = this.state[key];
-      }
+        buttonRef.current.blur();
     }
-    if (feed["url"] == null || feed["url"] == "") {
-      this.err.fadeIn("需要网址数据");
-      setTimeout(() => {this.err.fadeOut()}, 800);
+
+    updateField(k, v) {
+        this.setState({[k]: v});
     }
-    else {
-      this.err.fadeIn("正在提交 .....");
 
-      let item_content_xpath = feed["item_content_xpath"];
-      if (!item_content_xpath) {
-        delete feed["item_content_xpath"];
-      }
+    render() {
+        const style = {
+            borderWidth: 0,
+            paddingLeft: "36px"
+        };
 
-      let nodes = feed["removed_xpath_nodes"]
-      if (!nodes) {
-         delete feed["removed_xpath_nodes"];
-      }
-      let css = feed["css"];
-      if (!css) {
-        delete feed["css"]
-      }
-
-      let that = this;
-      fetch("/submit/rss", {
-        method: "POST",
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(feed)
-      })
-      .then(function(response) {
-        response.json().then(function(data) {
-          if (response.status == 200) {
-            that.err.fadeIn("成功");
-            setTimeout(() => {that.err.fadeOut()}, 800);
-            let state = that.getInitialState();
-            state["category"] = that.state.category;
-            that.setState(state);
-          }
-          else {
-            that.err.fadeIn("失败: " + data["message"]);
-            setTimeout(() => {that.err.fadeOut()}, 800);
-          }
-        })
-      })
-      .catch(function(err) {
-        that.err.fadeIn("异常: fetch exception");
-        setTimeout(() => {that.err.fadeOut()}, 800);
-      })
+        return (
+            <div>
+            <ErrMsg ref={(com) => this.err = com} />
+            <form onSubmit={this.submit} style={style}>
+            <EditBox desc="网址:" updateField={this.updateField} type="url" field="url" value={this.state.url} />
+            <SelectBox desc="类别:" updateField={this.updateField} field="category" url="/api/categories" value={this.state.category} />
+            <EditBox desc="内容selector[用于非全文输出的feed](选填):" updateField={this.updateField} type="text" field="item_content_xpath" value={this.state.item_content_xpath} />
+            <MEditBox desc="清除xpath node 数组(选填):" updateField={this.updateField} type="text" field="removed_xpath_nodes" value={this.state.removed_xpath_nodes} />
+            <TextBox desc="CSS:(选填):" updateField={this.updateField} type="text" field="css" value={this.state.css} />
+            <Button ref={buttonRef}/>
+            </form>
+            </div>
+        )
     }
-    findDOMNode(this.refs.Button).blur();
-  }
-
-  updateField(k, v) {
-    this.setState({[k]: v});
-  }
-
-  render() {
-    const style = {
-      borderWidth: 0,
-      paddingLeft: "36px"
-    };
-
-    return (
-      <div>
-        <ErrMsg ref={(com) => this.err = com} />
-        <form onSubmit={this.submit} style={style}>
-          <EditBox desc="网址:" updateField={this.updateField} type="url" field="url" value={this.state.url} />
-          <SelectBox desc="类别:" updateField={this.updateField} field="category" url="/api/categories" value={this.state.category} />
-          <EditBox desc="内容selector[用于非全文输出的feed](选填):" updateField={this.updateField} type="text" field="item_content_xpath" value={this.state.item_content_xpath} />
-          <MEditBox desc="清除xpath node 数组(选填):" updateField={this.updateField} type="text" field="removed_xpath_nodes" value={this.state.removed_xpath_nodes} />
-          <TextBox desc="CSS:(选填):" updateField={this.updateField} type="text" field="css" value={this.state.css} />
-          <Button ref="Button"/>
-        </form>
-      </div>
-    )
-  }
 }
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Title title="源(rss|atom)" />
-        <Hr />
-        <SubmitForm />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+            <Title title="源(rss|atom)" />
+            <Hr />
+            <SubmitForm />
+            </div>
+        )
+    }
 }
 
 const node = document.getElementById("content");
 const root = createRoot(node);
 root.render(<App />);
+
+
+/* vim: set ts=4 sw=4 sts=4 ft=javascript et: */
