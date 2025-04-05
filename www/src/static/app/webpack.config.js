fd70e23fce71c4e6027afaa397891cf4dbee9738
code4hack <code4hack+github@gmail.com>
2025-04-05 12:11:04 +0800

    spider

--- www/src/static/app/webpack.config.js
+++ www/src/static/app/webpack.config.js
@@ -29,11 +29,6 @@ module.exports = {
                     presets: [
                         '@babel/preset-env',
                         '@babel/preset-react',
-                        {
-                            plugins: [
-                                '@babel/plugin-transform-class-properties'
-                            ]
-                        }
                     ]
                 },
             }
