4fd5d5afad0b069cb8eb7f1061de2dff12f6bc9e
code4hack <code4hack+github@gmail.com>
2025-04-05 12:04:02 +0800

    spider

--- www/src/static/app/webpack.config.js
+++ www/src/static/app/webpack.config.js
@@ -5,45 +5,50 @@ const configuration = {};
 const jspath = path.resolve(__dirname);
 
 module.exports = {
-  entry: {
-    day: jspath + "/day.js",
-    entries: jspath + "/entries.js",
-    rss: jspath + "/rss.js",
-    spiders: jspath + "/spiders.js"
-  },
+    entry: {
+        day: jspath + "/day.js",
+        entries: jspath + "/entries.js",
+        rss: jspath + "/rss.js",
+        spiders: jspath + "/spiders.js"
+    },
 
-  output: {
-    path: jspath + "/../script/",
-    filename: "[name].js",
-  },
+    output: {
+        path: jspath + "/../script/",
+        filename: "[name].js",
+        clean: true,
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.js/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-
-        options: {
-          presets: [
-            '@babel/preset-env',
-            '@babel/preset-react',
+    module: {
+        rules: [
             {
-              plugins: [
-                '@babel/plugin-transform-class-properties'
-              ]
-            }
-          ]
-        },
-      }
-    ]
-  },
+                test: /\.js/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
 
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
-    }),
-  ],
+                options: {
+                    presets: [
+                        '@babel/preset-env',
+                        '@babel/preset-react',
+                        {
+                            plugins: [
+                                '@babel/plugin-transform-class-properties'
+                            ]
+                        }
+                    ]
+                },
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Production',
+        }),
+    ],
+
+   mode: 'development',
+   devtool: 'inline-source-map',
 };
+
+
+/* vim: set ts=4 sw=4 sts=4 ft=javascript et: */
